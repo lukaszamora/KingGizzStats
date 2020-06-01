5e76b9ea7a440f5c867a1b52e0b325f080e4eaee
Lukas Zamora <lz975545@tamu.edu>
2020-06-01 15:16:45 -0500

    fixed plots

--- Script.R
+++ Script.R
@@ -37,8 +37,8 @@ library(viridis)
 library(hrbrthemes)
 # create a table of year/album/number of songs
 album_years <- c(2012,2013,2013,2014,2014,2015,2015,2016,2017,2017,2017,2017,2017,2019,2019)
-album_name <- c("12 Bar Bruise","Eyes Like The Sky","Float Along - Fill Your Lungs","Oddments","I'm In Your Mind Fuzz", "Paper Mâché Dream Balloon", "Quarters!","Nonagon Infinity", "Flying Microtonal Banana","Murder of the Universe","Sketches of Brunswick East","Polygondwanaland","Gumboot Soup","Fishing For Fishies","Infest The Rats' Nest")
-album_num_songs <- c(12,10,8,12,10,4,12,9,9,21,13,10,11,9,9)
+album_name <- c("12 Bar","Eyes","Float Along","Oddments","Mind Fuzz", "Paper Mâché", "Quarters!","Nonagon", "FMB","MotU","Sketches","Poly","Gumboot","Fishies","Rats' Nest")
+album_num_songs <- c(12,10,8,12,10,12,4,9,9,21,13,10,11,9,9)
 albums_df <- data.frame(album_years,album_name,album_num_songs)
 
 # plot
@@ -51,60 +51,66 @@ ggplot(albums_df, aes(fill=album_name, y=album_num_songs, x=album_years)) +
 ggplotly()
 
 
-# Plot average track length over time (with regression line)
-# get average track length for each year
-song_length_2012 <- mean(twelve_bar$length)
-song_length_2013 <- mean(mean(eyes$length) + mean(float_along$length))/2
-song_length_2014 <- mean(mean(oddments$length) + mean(mind_fuzz$length))/2
-song_length_2015 <- mean(mean(quart$length) + mean(paper_mache$length))/2
-song_length_2016 <- mean(nonagon$length)
-song_length_2017 <- mean(mean(fmb$length) + mean(motu$length) + mean(sketches$length) + mean(poly$length) + mean(gumboot$length))/5
-song_length_2019 <- mean(mean(fishies$length) + mean(rats_nest$length))/2
+# Average track length over time
+song_length_df <- data.frame("album" = c("12 Bar","Eyes","Float ALong",
+                                         "Oddments","Mind Fuzz","Quarters",
+                                         "Paper Mache", "Nonagon", "FMB",
+                                         "MotU", "Sketches", "Poly", "Gumboot",
+                                         "Fishies", "Rats' Nest"),
+                             "avg track length" = c(mean(twelve_bar$length),mean(eyes$length),
+                                                    mean(float_along$length),mean(oddments$length),
+                                                    mean(mind_fuzz$length),mean(quart$length),
+                                                    mean(paper_mache$length),mean(nonagon$length),
+                                                    mean(fmb$length),mean(motu$length),mean(sketches$length),
+                                                    mean(poly$length),mean(gumboot$length),
+                                                    mean(fishies$length),mean(rats_nest$length) ))
+# order album by release
+song_length_df$album <- factor(song_length_df$album, levels = song_length_df$album)
 
-# plot
-song_length_df <- data.frame("year" = c(2012,2013,2014,2015,2016,2017,2019), "avg track length" = c(song_length_2012,song_length_2013,song_length_2014,song_length_2015,song_length_2016,song_length_2017,song_length_2019))
-ggplot(song_length_df, aes(x=year, y=avg.track.length)) +
+#plot
+ggplot(song_length_df, aes(x=album, y=avg.track.length,group=1)) +
   geom_point() +
-  geom_line() +
-  geom_smooth(method = "glm", se=FALSE) +
-  ggtitle("Average Track Length by Year") +
-  xlab("year") + ylab("avg track length (minutes)") +
-  scale_x_continuous("year", labels = as.character(album_years), breaks = album_years)
+  geom_smooth(method = "lm", se=FALSE, formula = y ~ log(x)) +
+  ggtitle("Average Track Length by Album") +
+  xlab("album") + ylab("avg track length (minutes)")
 ggplotly()
 
 
-# Find King Gizz's happiest sounding song ( use sonic happy index = \sqrt(valence*energy) )
+# Happiest sounding song ( use sonic happy index = \sqrt(valence*energy) )
 happy_index <- (data$valence*data$energy)^.5
 happy_df <- data.frame(data$name, happy_index)
 happy_df %>% arrange(desc(happy_index))
-p <- ggplot(data=happy_df, aes(x=data$name, y=happy_index, colour=data$album)) +
+
+ggplot(data=happy_df, aes(x=data$name, y=happy_index, colour=data$album)) +
   geom_point() +
   theme(axis.text.x=element_blank(),axis.ticks.x=element_blank()) +
   ggtitle("Happiest Sounding Songs") +
   ylab("happy index") + xlab("song")
-ggplotly(p)
+
 
 # Most energetic sounding song
 energy_index <- data$energy
 energy_df <- data.frame(data$name, energy_index)
 energy_df %>% arrange(desc(energy_index))
-pl <- ggplot(data=energy_df, aes(x=data$name, y=energy_index, colour=data$album)) +
+
+ggplot(data=energy_df, aes(x=data$name, y=energy_index, colour=data$album)) +
   geom_point() +
   theme(axis.text.x=element_blank(),axis.ticks.x=element_blank()) +
   ggtitle("Most Energetic Sounding Songs") +
   ylab("energy index") + xlab("song")
-ggplotly(pl)
+
 
 # Find King Gizz's angriest sounding song ( use sonic happy index = \sqrt(1-valence*energy) )
 anger_index <- ((1-data$valence)*data$energy)^.5
 anger_df <- data.frame(data$name, anger_index)
 anger_df %>% arrange(desc(anger_index))
-fig <- ggplot(data=anger_df, aes(x=data$name, y=anger_index, colour=data$album)) +
+
+ggplot(data=anger_df, aes(x=data$name, y=anger_index, colour=data$album)) +
   geom_point() +
   theme(axis.text.x=element_blank(),axis.ticks.x=element_blank()) +
   ggtitle("Angriest Sounding Songs") +
   ylab("anger index") + xlab("song")
-ggplotly(fig)
+
 
 # Plot avg danceability/energy over time
 avg_energy <- data.frame(album_name, c(mean(twelve_bar$energy),
@@ -123,23 +129,14 @@ avg_energy <- data.frame(album_name, c(mean(twelve_bar$energy),
                                        mean(fishies$energy),
                                        mean(rats_nest$energy)))
 colnames(avg_energy) <- c("album_name", "avg_energy")
-avg_energy
+avg_energy$album_name <- factor(avg_energy$album_name, levels = avg_energy$album_name)
 
-e <- ggplot(avg_energy, aes(x=album_name, y=avg_energy)) +
+ggplot(avg_energy, aes(x=album_name, y=avg_energy,group=1)) +
   geom_point() +
-  geom_line() +
-  geom_smooth(method = lm, se=FALSE) +
-  labs(title = "Effect of Vitamin C on Tooth Growth",
-       subtitle = "Plot of length by dose") +
-  xlab("album") + ylab("average energy") +
-  theme(axis.text.x=element_blank(),
-        axis.ticks.x=element_blank())
-ggplotly(e) %>%
-  layout(title = list(text = paste0('Average Energy per Album',
-                                    '<br>',
-                                    '<sup>',
-                                    'Sorted by Alphabetical Order',
-                                    '</sup>')))
+  stat_smooth(method = 'lm', aes(x=album_name, y=avg_energy), se = FALSE, formula = y ~ poly(x,3)) +
+  labs(title = "Average Energy per Album", subtitle = "Sorted by Release Date") +
+  xlab("album") + ylab("average energy")
+ggplotly()
 
 f <- avg_dance <- data.frame(album_name, c(mean(twelve_bar$danceability),
                                        mean(eyes$danceability),
@@ -157,18 +154,11 @@ f <- avg_dance <- data.frame(album_name, c(mean(twelve_bar$danceability),
                                        mean(fishies$danceability),
                                        mean(rats_nest$danceability)))
 colnames(avg_dance) <- c("album_name", "avg_danceability")
+avg_dance$album_name <- factor(avg_dance$album_name, levels = avg_dance$album_name)
 
-f <- ggplot(avg_dance, aes(x=album_name, y=avg_danceability)) +
+ggplot(avg_dance, aes(x=album_name, y=avg_danceability,group=1)) +
   geom_point() +
-  geom_line() +
-  geom_smooth(method = lm, se=FALSE) +
-  ggtitle("Average Danceability per Album") +
-  xlab("album") + ylab("average danceability") +
-  theme(axis.text.x=element_blank(),
-        axis.ticks.x=element_blank())
-ggplotly(f) %>%
-  layout(title = list(text = paste0('Average Danceability per Album',
-                                    '<br>',
-                                    '<sup>',
-                                    'Sorted by Alphabetical Order',
-                                    '</sup>')))
+  stat_smooth(method = 'lm', aes(x=album_name, y=avg_danceability), se = FALSE, formula=y~poly(x,3)) +
+  labs(title = "Average Danceability per Album", subtitle = "Sorted by Release Date") +
+  xlab("album") + ylab("average danceability")
+ggplotly()
